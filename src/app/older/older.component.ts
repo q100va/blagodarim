080c86668f64f25dc9146e535f3977c10741da19
Oksana Kustova <q100wa@gmail.com>
2025-01-31 09:18:17 -0500

    changed forms.

--- src/app/older/older.component.ts
+++ src/app/older/older.component.ts
@@ -76,7 +76,7 @@ export class OlderComponent implements OnInit {
 
   ngAfterViewInit(): void {
     let img = new Image();
-    img.src = 'assets/newYear2025_1.jpg';
+    img.src = 'assets/winter3.jpg';
     let ctx = this.myCanvas.nativeElement.getContext('2d');
     let myCanvas = this.myCanvas.nativeElement;
     let submit = this.submit.nativeElement;
