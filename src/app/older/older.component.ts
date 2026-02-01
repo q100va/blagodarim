836e940617e9c001d4a5924adb40820abe34495d
q100va <q100wa@gmail.com>
2026-01-31 22:21:35 -0500

    spring 2026

--- src/app/older/older.component.ts
+++ src/app/older/older.component.ts
@@ -76,7 +76,7 @@ export class OlderComponent implements OnInit {
 
   ngAfterViewInit(): void {
     let img = new Image();
-    img.src = 'assets/newYear2026_6.jpg';
+    img.src = 'assets/spring2026_2.jpg';
     let ctx = this.myCanvas.nativeElement.getContext('2d');
     let myCanvas = this.myCanvas.nativeElement;
     let submit = this.submit.nativeElement;
